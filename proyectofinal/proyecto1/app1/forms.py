a1dc677da7fe10941c51dda73576bce9ad929bf1
gasparferro <130009781+gasparferro@users.noreply.github.com>
2023-05-20 13:05:48 -0300

    Add files via upload

---
+++ proyectofinal/proyecto1/app1/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class cursoformulario(forms.Form):
+    nombre = forms.CharField()
+    raza = forms.CharField()
+
+class Creacionformulario(forms.Form):
+    nombre = forms.CharField(max_length=30, required=False)
+    genero = forms.CharField(max_length=30, required=False)
+    profesion = forms.CharField(max_length=30, required=False)
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Repetir contraseña", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        # Saca los mensajes de ayuda
+        help_texts = {k:"" for k in fields}
+
+class LoginForm(forms.Form):
+    usuario = forms.CharField (max_length=30)
+    password = forms.CharField (max_length=30)
+
+
+
